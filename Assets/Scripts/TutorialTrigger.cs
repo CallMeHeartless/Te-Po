76779abd52b37b5385b9822d8bf87cf243dfa0fd
Kerry <k.of.blades@gmail.com>
2018-08-15 17:15:07 +1200

    Nothing of value

---
+++ Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialTrigger : MonoBehaviour {
+    public GameObject whetuUIText;
+    public string tutorialText;
+
+	// Use this for initialization
+	void Start () {
+        //whetuUIText = GameObject.Find("WhetuUI/Text").GetComponent<GameObject>();
+
+        Debug.Log(whetuUIText.name);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Enable UI text and display
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            // Display and enable text
+            whetuUIText.GetComponent<Text>().text = tutorialText;
+            whetuUIText.SetActive(true);
+        }
+    }
+
+    // Disable UI text
+    void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            // Disable text
+            whetuUIText.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/WhetuUIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhetuUIController : MonoBehaviour {
+
+    public Vector3 offset;
+    public GameObject Whetu;
+
+	// Use this for initialization
+	void Start () {
+        Whetu = GameObject.Find("Whetu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Whetu.transform.position + offset;
+	}
+}
