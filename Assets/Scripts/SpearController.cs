bba8892599e65c14569d7187086b1cbfc505d813
Kerry <k.of.blades@gmail.com>
2018-08-22 09:51:13 +1200

    Implemented spear controller

---
+++ Assets/Scripts/SpearController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearController : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            PlayerController.Kill();
+        }
+    }
+}
