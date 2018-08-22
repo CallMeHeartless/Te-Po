6546964d6d6a12ceeb35ff885a5afce83da1f69b
Kerry <k.of.blades@gmail.com>
2018-08-22 15:46:14 +1200

    Created ruru call trigger

---
+++ Assets/Scripts/RuruCallTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuruCallTrigger : MonoBehaviour {
+
+    public bool isWarning;
+    private bool hasCalled = false;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player") && !hasCalled) {
+            if (isWarning) {
+                RuruCallManager.WarningCall();
+            } else {
+                RuruCallManager.SafeCall();
+            }
+
+            hasCalled = true;
+        }
+    }
+
+}
